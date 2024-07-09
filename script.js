2fdea0b115961b862fc092cf7e3936b19eb510ce
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-07-09 15:14:38 +0530

    add: dark theme switch 🌓

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const themeSwitch = document.getElementById("theme-switch");
+
+
+
+const toggleTheme = (e) => {
+    if(e.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+themeSwitch.addEventListener("change",toggleTheme);
+
+
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        themeSwitch.checked = true;
+    }
+}
